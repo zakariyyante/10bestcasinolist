3e37ae87900284ad6f2af9cc9b2ae2f4cf737c15
Zakariyya <zakariyya.z@antetechnologies.com>
2026-06-11 12:40:46 +0400

    tets

--- components/MobileModal.tsx
+++ components/MobileModal.tsx
@@ -22,7 +22,7 @@ declare global {
 const isMobileDevice = () => {
   if (typeof window === 'undefined') return false;
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-    || window.innerWidth < 768;
+    && window.matchMedia('(max-width: 767px)').matches;
 };
 
 export const MobileModal = ({ brands, gclid }: MobileModalProps) => {
